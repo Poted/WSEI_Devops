01b5d9170e3cd9f55db1d18fc750ae6919410362
Poted <o.jpkmc@gmail.com>
2026-01-06 23:09:34 +0100

    feat: add redis counter

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from flask import Flask, jsonify
 import datetime
 import os
+import redis
 
 app = Flask(__name__)
 
+try:
+    cache = redis.Redis(host='redis-db', port=6379, decode_responses=True)
+except Exception as e:
+    cache = None
+
 @app.route('/')
 def home():
+    count = "Redis connection not available"
+    
+    if cache:
+        try:
+            count = cache.incr('hits')
+        except redis.exceptions.ConnectionError:
+            pass
+
     return jsonify({
         "status": "running",
         "service": "devops-project-app",
-        "timestamp": str(datetime.datetime.now())
+        "timestamp": str(datetime.datetime.now()),
+        "visitor_count": count
     })
 
 @app.route('/info')
